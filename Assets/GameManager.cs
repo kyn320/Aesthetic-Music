636d3914f5ec42bf81456f4fe0b04ce3ce2e440b
kyn320 <kyn320@naver.com>
2017-04-17 19:54:45 +0900

    first commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+
+    public float bpm;
+    public float currentTime;
+
+    public float speed;
+    public float sync;
+
+    public Transform judgeLine;
+
+    public GameObject note;
+
+
+    //어떻게 만들죠?
+    //음 노트가 어떻게 해야 움직이게 할까요.
+    // noteX = (판정선의 X좌표) + (노트 시간 - (현재 시간 + 싱크) ) * 속도  * (곡의 BPM / 기준 BPM(60) )
+    //  움직임은 Position = new Vector(x,y,z); 
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        Instantiate(note);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void FixedUpdate()
+    {
+        currentTime += Time.fixedDeltaTime;
+    }
+
+
+
+
+}
---
+++ Assets/Note.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour {
+
+    public static GameManager gameManager;
+
+    public float noteTime;
+
+
+    public int dir = -1;
+    private Vector2 notePos;
+    private Transform tr;
+
+    private void Awake()
+    {
+        gameManager = GameManager.instance;
+        tr = GetComponent<Transform>();
+        
+    }
+
+
+    private void FixedUpdate()
+    {
+        notePos.x = gameManager.judgeLine.position.x + ( dir * (noteTime - (gameManager.currentTime + gameManager.sync)) * gameManager.speed * (gameManager.bpm / 60) ) ;
+        tr.position = notePos;
+
+    }
+}
